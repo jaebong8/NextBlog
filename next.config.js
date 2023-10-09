11e9427a6b293297a284512da1b36b52fde1fcc0
jaebong8 <jaebong90@naver.com>
2023-10-09 21:16:27 +0900

    redirect 설정

--- next.config.js
+++ next.config.js
@@ -3,6 +3,20 @@ const nextConfig = {
     images: {
         remotePatterns: [{ protocol: 'https', hostname: 'images.unsplash.com' }],
     },
+    async redirects() {
+        return [
+            {
+                source:"/posts/deleted_forever",
+                destination:"/",
+                permanent: true,
+            },
+            {
+                source:"/posts/deleted_temp",
+                destination:"/",
+                permanent: false,
+            }
+        ]
+    }
 };
 
 module.exports = nextConfig;
